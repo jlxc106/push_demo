05630bf30292f3e9a6dd812ee3d4fd2ccf2d01fd
Jay Lim <jaylim106@gmail.com>
2018-09-19 21:11:00 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.use(express.static(path.resolve(__dirname, 'public')))
+
+
+app.listen(3010, () => {
+    console.log('listening on port 3000')
+})
